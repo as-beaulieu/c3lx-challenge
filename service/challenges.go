c9c1e5dececcf4e434b0085a7c7677ada357a11c
Andrew Beaulieu <as.beaulieu@gmail.com>
2021-01-27 21:09:57 -0700

    filler service logic to test new challenges endpoints

--- service/challenges.go
+++ service/challenges.go
@@ -6,3 +6,16 @@ type ChallengesOperator interface {
 	GetChallenges() ([]models.Challenge, error)
 	AcceptChallenge(request models.AcceptChallengeRequest) (models.Challenge, error)
 }
+
+func (s service) GetChallenges() ([]models.Challenge, error) {
+	return []models.Challenge{
+		{
+			Name:        "Not Implemented",
+			Description: "Not Implemented",
+		},
+	}, nil
+}
+
+func (s service) AcceptChallenge(request models.AcceptChallengeRequest) (models.Challenge, error) {
+	return models.Challenge{Name: "Not Implemented", Description: "Not Implemented"}, nil
+}
