3c76731b88de064250878fd910b0a3d971bef789
Andrew Beaulieu <as.beaulieu@gmail.com>
2021-01-27 21:13:52 -0700

    changed post to parrot request data for testing

--- service/challenges.go
+++ service/challenges.go
@@ -17,5 +17,5 @@ func (s service) GetChallenges() ([]models.Challenge, error) {
 }
 
 func (s service) AcceptChallenge(request models.AcceptChallengeRequest) (models.Challenge, error) {
-	return models.Challenge{Name: "Not Implemented", Description: "Not Implemented"}, nil
+	return models.Challenge{Name: request.Name, Description: "Not Implemented"}, nil
 }
