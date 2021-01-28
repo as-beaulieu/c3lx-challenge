a5c51899f97a8a45eafbe2fa6ee3eb0e6a52ef66
Andrew Beaulieu <as.beaulieu@gmail.com>
2021-01-27 21:53:39 -0700

    service call for GetChallenges()

--- service/challenges.go
+++ service/challenges.go
@@ -1,19 +1,27 @@
 package service
 
-import "c3lx-challenge/src/models"
+import (
+	"c3lx-challenge/src/models"
+	"go.uber.org/zap"
+)
 
 type ChallengesOperator interface {
-	GetChallenges() ([]models.Challenge, error)
+	GetChallenges() ([]*models.Challenge, error)
 	AcceptChallenge(request models.AcceptChallengeRequest) (models.Challenge, error)
 }
 
-func (s service) GetChallenges() ([]models.Challenge, error) {
-	return []models.Challenge{
-		{
-			Name:        "Not Implemented",
-			Description: "Not Implemented",
-		},
-	}, nil
+func (s service) GetChallenges() ([]*models.Challenge, error) {
+	logger := s.logger.Named("s.GetChallenges")
+	logger.Info("Getting all challenges from dao")
+
+	results, err := s.postgres.GetAllChallenges()
+	if err != nil {
+		logger.Error("error calling dao", zap.Error(err))
+		return nil, err
+	}
+
+	logger.Info("successful return of challenges")
+	return results, nil
 }
 
 func (s service) AcceptChallenge(request models.AcceptChallengeRequest) (models.Challenge, error) {
